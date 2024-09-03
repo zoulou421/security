8216db65125ad118d8efc39fddb4997197d555fa
Bonevy BEBY <bonevybeby@gmail.com>
2024-09-03 08:11:15 +0200

    all servlets tested ok

--- src/main/java/com/groupekilo/security/controller/WelcomeServlet.java
+++ src/main/java/com/groupekilo/security/controller/WelcomeServlet.java
@@ -1,5 +1,17 @@
 package com.groupekilo.security.controller;
 
-public class WelcomeServlet {
+import java.io.IOException;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "welcome", value = "/welcome")
+public class WelcomeServlet extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+       req.getRequestDispatcher("WEB-INF/jsp/welcome.jsp").forward(req, resp);
+	}
 }
--- src/main/webapp/WEB-INF/jsp/users/list.jsp
+++ src/main/webapp/WEB-INF/jsp/users/list.jsp
@@ -7,6 +7,6 @@
 <title>Insert title here</title>
 </head>
 <body>
-
+liste
 </body>
 </html>
