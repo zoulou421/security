0f253154f33e339800507171eaa8fff9f27a6bab
Bonevy BEBY <bonevybeby@gmail.com>
2024-09-03 07:56:40 +0200

    test login works ok

---
+++ src/main/java/com/groupekilo/security/controller/AdminServlet.java
@@ -0,0 +1,5 @@
+package com.groupekilo.security.controller;
+
+public class AdminServlet {
+
+}
---
+++ src/main/java/com/groupekilo/security/controller/LoginServlet.java
@@ -0,0 +1,28 @@
+package com.groupekilo.security.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "login", value = "/login")
+public class LoginServlet extends HttpServlet {
+	
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		
+	}
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+       req.getRequestDispatcher("index.jsp").forward(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+	}
+}
