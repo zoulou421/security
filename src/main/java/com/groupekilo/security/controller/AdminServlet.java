97c8a0fea182d8a37713cd73ee78f45d61eae43b
Bonevy BEBY <bonevybeby@gmail.com>
2024-09-05 00:57:19 +0200

    added: service into SERVLET/best PRACTICE

--- src/main/java/com/groupekilo/security/controller/AdminServlet.java
+++ src/main/java/com/groupekilo/security/controller/AdminServlet.java
@@ -11,21 +11,26 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.groupekilo.security.dto.UserDto;
+import com.groupekilo.security.service.IUserService;
+import com.groupekilo.security.service.UserService;
 
 @WebServlet(name = "admin", value = "/admin")
 public class AdminServlet extends HttpServlet {
+	IUserService userService=new UserService();
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		List<UserDto>users=new ArrayList<UserDto>();
-		
+		/*
+		 List<UserDto>users=new ArrayList<UserDto>();
+		 
 		UserDto user1=new UserDto(1,"Bonevy","BEBY","bonevy@formationkilo.com");
 		UserDto user2=new UserDto(2,"Natasha","BEBY","natasha@formationkilo.com");
 		UserDto user3=new UserDto(3,"Ketsia","BEBY","ketsia@formationkilo.com");
-
 		users.add(user1);
 		users.add(user2);
-		users.add(user3);
-		req.setAttribute("users", users);
+		users.add(user3);*/
+		
+		//req.setAttribute("users", users);
+		req.setAttribute("users", userService.getAll());
 		req.getRequestDispatcher("WEB-INF/jsp/users/list.jsp").forward(req, resp);
 	}
 
